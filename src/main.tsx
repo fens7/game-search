ae0e44509afaf10269cedd0f777d45402b811f4c
Maxym Zavadovych <zavadovich17@gmail.com>
2024-04-25 19:09:14 +0300

    implemented dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import App from './App.tsx';
 import './index.css';
 
 import 'bootstrap/dist/css/bootstrap.css';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import theme from './theme.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <App />
     </ChakraProvider>,
 );
