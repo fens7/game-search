6bf523c5ff8e69c9f7fb39c6171811544b3a9642
Maxym Zavadovych <zavadovich17@gmail.com>
2024-04-25 18:17:32 +0300

    Main page responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import { Grid, GridItem, Show } from '@chakra-ui/react';
 
 function App() {
-    return <></>;
+    return (
+        <>
+            <Grid
+                templateAreas={{
+                    base: `'nav' 'main'`,
+                    lg: `'nav nav' 'aside main'`, //1024px
+                }}>
+                <GridItem area="nav" bg={'orange'}>
+                    Nav
+                </GridItem>
+                <Show above="lg">
+                    <GridItem area="aside" bg={'blue'}>
+                        Aside
+                    </GridItem>
+                </Show>
+                <GridItem area="main" bg={'black'}>
+                    Main
+                </GridItem>
+            </Grid>
+        </>
+    );
 }
 
 export default App;
