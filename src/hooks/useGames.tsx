f1301f44e716bd19eff2dfc25bb5eb20d9dd4e50
Maxym Zavadovych <zavadovich17@gmail.com>
2024-04-27 16:51:53 +0300

    fetch hook

---
+++ src/hooks/useGames.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import apiClient from '../services/api-client';
+
+interface Game {
+    id: number;
+    name: string;
+}
+
+interface FetchGamesResponse {
+    count: number;
+    results: Game[];
+}
+
+const useGames = () => {
+    const [games, setGames] = React.useState<Game[]>([]);
+    const [error, setError] = React.useState('');
+
+    const controller = new AbortController();
+
+    React.useEffect(() => {
+        apiClient
+            .get<FetchGamesResponse>('/games', { signal: controller.signal })
+            .then((res) => setGames(res.data.results))
+            .catch((err) => setError(err.message));
+
+        return () => {
+            controller.abort();
+        };
+    }, []);
+
+    return { games, error };
+};
+
+export default useGames;
