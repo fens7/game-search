a4583dcb6e4af845541159c17165e7b0673a4594
Maxym Zavadovych <zavadovich17@gmail.com>
2024-07-31 23:13:22 +0300

    game no image handle

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImg from '../assets/no-image-placeholder.webp';
+
 const getCroppedImg = (url: string) => {
-    if (!url) return '';
+    if (!url) return noImg;
     const index = url.indexOf('media/') + 'media/'.length;
     return url.slice(0, index) + 'crop/600/400/' + url.slice(index, url.length);
 };
